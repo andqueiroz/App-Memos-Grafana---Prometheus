bad291ce4be65b58a42affd4f3cbd9378e20d79e
André <and.queiroz@hotmail.com>
2026-03-12 00:27:49 -0300

    fix: formata os blocos de security groups no main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,9 +21,9 @@ data "aws_subnets" "default" {
 # Certificado Autoassinado
 resource "tls_private_key" "lab_key" { algorithm = "RSA" }
 resource "tls_self_signed_cert" "lab_cert" {
-  private_key_pem = tls_private_key.lab_key.private_key_pem
+  private_key_pem       = tls_private_key.lab_key.private_key_pem
   validity_period_hours = 8760
-  allowed_uses = ["key_encipherment", "digital_signature", "server_auth"]
+  allowed_uses          = ["key_encipherment", "digital_signature", "server_auth"]
   subject { common_name = "meulab.local" }
 }
 resource "aws_acm_certificate" "cert" {
@@ -35,52 +35,48 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_security_group" "alb_sg" {
   name        = "alb_sg_lab"
   description = "Permite HTTPS externo"
-  ingress { from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
-  egress  { from_port = 0, to_port = 0, protocol = "-1", cidr_blocks = ["0.0.0.0/0"] }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2_k3s_sg_lab"
   description = "Permite trafego do ALB para a EC2"
-  ingress { from_port = 80, to_port = 80, protocol = "tcp", security_groups = [aws_security_group.alb_sg.id] }
-  egress  { from_port = 0, to_port = 0, protocol = "-1", cidr_blocks = ["0.0.0.0/0"] }
-}
 
-# Application Load Balancer
-resource "aws_lb" "lab_alb" {
-  name               = "lab-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = data.aws_subnets.default.ids
-}
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
 
-resource "aws_lb_target_group" "k3s_tg" {
-  name     = "k3s-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.lab_alb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.cert.arn
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.k3s_tg.arn
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 # Instância EC2
 resource "aws_instance" "k3s_node" {
-  ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 em us-east-1
-  instance_type = "t3.medium"
+  ami                    = "ami-0c7217cdde317cfec" # Ubuntu 22.04 em us-east-1
+  instance_type          = "t3.medium"
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  user_data     = file("userdata.sh")
-  
+  user_data              = file("userdata.sh")
+
   tags = { Name = "Lab-K3s-Server" }
 }
 
@@ -91,6 +87,6 @@ resource "aws_lb_target_group_attachment" "attach_ec2" {
 }
 
 output "url_acesso" {
-  value = "https://${aws_lb.lab_alb.dns_name}"
+  value       = "https://${aws_lb.lab_alb.dns_name}"
   description = "Acesse suas aplicações por este link"
 }
